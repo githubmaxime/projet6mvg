c56631c3f37cc92e19de7088034faf12fb21355a
Maxime <max62217@hotmail.fr>
2024-10-20 19:25:11 +0200

    Base backend

---
+++ backend/routes/book.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config')
+
+const bookCtrl = require('../controllers/book');
+
+router.post('/', auth, multer, bookCtrl.createBook);
+router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
+router.get('/:id', bookCtrl.getOneBook);
+router.get('/', bookCtrl.getAllStuff);
+
+module.exports = router;
